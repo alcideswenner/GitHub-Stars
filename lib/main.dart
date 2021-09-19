980c913ac542e4b4a58b306ab1b6a85e975aff73
Alcides Wenner <alcideswenner.jus@gmail.com>
2021-09-19 18:17:38 -0300

    Card Info User

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:github_stars/view/home.dart';
 import 'package:github_stars/view/splash_page_init.dart';
 
-void main() {
+void main() async {
+   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.black54,
+    statusBarIconBrightness: Brightness.light
+  ));
   runApp(MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backwardsCompatibility: false, // 1
+          systemOverlayStyle: SystemUiOverlayStyle.light, // 2
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       title: "GitHub Stars",
       initialRoute: 'init',
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -7,13 +7,55 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home>  {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarTextStyle: TextStyle(color: Colors.white),
+        backgroundColor: Colors.black87,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        centerTitle: true,
+        title: Text("GitHub Stars"),
+      ),
+      body: cardInfoUser(),
+    );
   }
 
   Widget cardInfoUser() {
-    return Container();
+    return Container(
+      height: 250,
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      //    width: MediaQuery.of(context).size.width - 50,
+      decoration: BoxDecoration(
+        color: Colors.black87,
+      ),
+      child: Container(
+          child: Column(
+        children: [
+          new Card(
+            child: new ListTile(
+              leading: new Icon(Icons.search),
+              title: new TextField(
+                maxLines: 1,
+                //controller: controller,
+                decoration: new InputDecoration(
+                    hintText: 'Buscar', border: InputBorder.none),
+                //onChanged: onSearchTextChanged,
+              ),
+              trailing: new IconButton(
+                icon: new Icon(Icons.cancel),
+                onPressed: () {
+                  // controller.clear();
+                  // onSearchTextChanged('');
+                },
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
